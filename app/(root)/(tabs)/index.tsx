8bd67aeb0f31c458be8a3de9da69c5ef5e3f86c4
Sabina Gasanova <sabina21gasanova@yandex.ru>
2025-03-25 15:12:28 +0300

    homepage in procces

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,18 +1,38 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Search from "@/components/Search";
 
 export default function Index() {
   return (
-    <SafeAreaView>
-      <View>
-        <Text className="font-bold text-2xl my-4 font-rubik">
-          Welcome to app
-        </Text>
-        <Link href="/sign-in">Sign In</Link>
-        <Link href="/explore">Explore</Link>
-        <Link href="/profile">Profile</Link>
-        <Link href="/properties/1">Property</Link>
+    <SafeAreaView className="h-full bg-white ">
+      <View className="px-5">
+        <View className="flex flex-row justify-between items-center my-4">
+          <View className="flex flex-row items-center gap-2">
+            <Image
+              source={images.avatar}
+              className="size-11"
+              resizeMode="contain"
+            />
+            <View className="flex flex-col items-start">
+              <Text className="text-black-100 text-base font-rubik">
+                Hello,
+              </Text>
+              <Text className="text-black-300 text-lg font-rubik-medium">
+                Pupa!
+              </Text>
+            </View>
+          </View>
+          <TouchableOpacity>
+            <Image
+              source={icons.bell}
+              className="size-6"
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        </View>
+        <Search />
       </View>
     </SafeAreaView>
   );
